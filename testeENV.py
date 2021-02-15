72e6152bf8c709e4954782bdb73460828d5a56e5
Gilberto <gilbertoliveir@gmail.com>
2021-02-15 09:40:31 -0300

    removendo test .env

--- testeENV.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-CONFIG_PATH = os.environ.get("CONFIG_PATH")
-DOMAIN = os.environ.get("DOMAIN")
-
-import pdb; pdb.set_trace()
