aade5bd278308c6b3fb8ec7fbb25f84b4c5a90c4
Gilberto <gilbertoliveir@gmail.com>
2021-02-17 10:15:14 -0300

    add

--- util.py
+++ util.py
@@ -1,13 +1,8 @@
 import re
 import json
+
 def removeDuplicado(valor):
     return list(dict.fromkeys(valor))
-
-# cnpj = "\d{2}.\d{3}.\d{3}/\d{4}-\d{2}"
-# email1 = r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"
-# tel1 = '\(\d{2}\)\s\d{4,5}\-\d{4}'
-# tel2 = '(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})'
-# cep = 'CEP\s*(\d{5})-(\d{3})'
     
 def regex(opcao,arquivo):
     tipoRegex={
